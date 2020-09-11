8cf9611dc5c546f2493ac7c0f8a6da98d86def9e
bukajj <42746240+bukajj@users.noreply.github.com>
2020-09-11 13:46:07 +0200

    Closing login tab test

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -13,12 +13,14 @@ context('Login and register tests', () => {
 
     it('Should login', () => {
         cy.login(users[0].email, users[0].password);
+
         cy.get('.main-menu__user--active')
         .should('be.visible');
     })
 
     it('Should not login', () => {
         cy.login(users[1].email, users[1].password);
+
         cy.get('.form-error')
         .should('contain', 'Please, enter valid credentials');
     })
@@ -26,8 +28,10 @@ context('Login and register tests', () => {
     it('Should display an error when email is empty', () => {
         cy.get('[data-testid=login-btn]')
         .click();
+
         cy.get('input[name="email"]')
         .clear();
+        
         cy.get('.input__error')
         .should('contain', 'Please fill out this field.');
     })
@@ -35,10 +39,63 @@ context('Login and register tests', () => {
     it('Should display an error when password is empty', () => {
         cy.get('[data-testid=login-btn]')
         .click();
+
         cy.get('input[name="password"]')
         .clear();
+
         cy.get('.input__error')
         .should('contain', 'Please fill out this field.');
+    })
+
+    it('Should "SIGN IN" button works properly', () => {
+        cy.get('[data-testid=login-btn]')
+        .click();
+
+        cy.get('input[name="email"]')
+        .clear()
+        .type(users[0].email);
+
+        cy.get('input[name="password"]')
+        .clear()
+        .type(users[0].password);
+        
+        cy.get('button[type="submit"]')
+        .click();
+
+        cy.get('.main-menu__user--active')
+        .should('be.visible');
+    })
+
+    it('Should password reminder contain text', () =>{
+        cy.get('[data-testid=login-btn]')
+        .click();
+
+        cy.get('.login__content__password-reminder')
+        .children()
+        .should('contain', 'Have you forgotten your password?');
+    })
+
+    it('Should forgot password button works properly', () => {
+        cy.get('[data-testid=login-btn]')
+        .click();
+
+        cy.get('.login__content__password-reminder > p')
+        .children('.u-link')
+        .click();
+
+        cy.get('p[class="overlay__header-text"]')
+        .should('contain','Reset your password');
+    })
+
+    it('Should close login tab', () => {
+        cy.get('[data-testid=login-btn]')
+        .click();
+        
+        cy.get('svg[data-src="/images/x.svg"]')
+        .click();
+
+        cy.get('.login')
+        .should('not.exist');
     })
 
 })
